f46101a0a7509449feb44c06a4689634c08517fa
repMsol <michael.meerholz105@gmail.com>
2025-07-07 00:47:46 +0200

    port

--- flask_server.py
+++ flask_server.py
@@ -73,4 +73,5 @@ def upload_file():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render stellt PORT bereit
+    app.run(host="0.0.0.0", port=port)
